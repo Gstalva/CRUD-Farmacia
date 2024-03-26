3f203aad3ef674a653164a2a47cbf72f9818bc27
Gstalva <gstalva1@gmail.com>
2024-03-26 12:16:20 -0300

    CRUD-Produto

--- farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
+++ farmacia/src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,11 +1,16 @@
 package com.generation.farmacia.model;
 
-import jakarta.persistence.Entity;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -28,6 +33,11 @@ public class Categoria {
 	
 	@NotBlank(message = "Informe se o medicamento é Generico ou não")
 	private String tipo;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
 
 	public Long getId() {
 		return id;
@@ -59,6 +69,14 @@ public class Categoria {
 
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 	
